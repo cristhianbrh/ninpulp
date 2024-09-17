bf85e2710cd60eb3eda28655dfb52f8f9be59e22
cristhianbrh <cristhianalexand@gmail.com>
2024-09-17 11:11:21 -0500

    Append escxene

--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     public Transform newCameraPosition;
+    public Transform previewCameraPosition;
+     private bool cameraAtNewPosition = false;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Camera.main.GetComponent<CameraMovement>().MoveCamera(newCameraPosition.position);
+             if (!cameraAtNewPosition) // Si la cámara no está en la nueva posición
+            {
+                Camera.main.GetComponent<CameraMovement>().MoveCamera(newCameraPosition.position);
+                cameraAtNewPosition = true; // Marca que la cámara ha cambiado a la nueva posición
+            }
+            else
+            {
+                Camera.main.GetComponent<CameraMovement>().MoveCamera(previewCameraPosition.position);
+                cameraAtNewPosition = false; // Marca que la cámara ha vuelto a la posición previa
+            }
         }
     }
 }
