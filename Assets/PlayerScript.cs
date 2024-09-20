548f1c7168f288366b44525b89987397a3d403fd
cristhianbrh <cristhianalexand@gmail.com>
2024-09-19 23:03:11 -0500

    Added animation(player)

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -12,11 +12,15 @@ public class PlayerScript : MonoBehaviour
     public float speed = 20f;
     public Vector2 direction;
     private Rigidbody2D rigidbody2D;
+    private Animator playerAnimator;
+    private SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -34,19 +38,19 @@ public class PlayerScript : MonoBehaviour
 
     private void MoveForDisp()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("Acción especial w activada");
         }
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("Acción especial A activada");
         }
-        if(Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             Debug.Log("Acción especial S activada");
         }
-        if(Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("Acción especial D activada");
         }
@@ -54,21 +58,40 @@ public class PlayerScript : MonoBehaviour
     private void Movement()
     {
         direction = Vector2.zero;
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            direction += Vector2.up;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            direction += Vector2.down;
-        }
+        playerAnimator.SetFloat("Vertical", 0f);
+        playerAnimator.SetFloat("Horizontal", 0f);
+        sr.flipY = false;
+
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             direction += Vector2.left;
+            playerAnimator.SetFloat("Horizontal", -1f);
+            playerAnimator.SetFloat("Vertical", 0f);
+            sr.flipX = true;
+            sr.flipY = false;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             direction += Vector2.right;
+            playerAnimator.SetFloat("Horizontal", 1f);
+            playerAnimator.SetFloat("Vertical", 0f);
+            sr.flipX = false;
+            sr.flipY = false;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector2.up;
+            playerAnimator.SetFloat("Vertical", 1f);
+            playerAnimator.SetFloat("Horizontal", 0f);
+            sr.flipY = false;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+            playerAnimator.SetFloat("Vertical", -1f);
+            playerAnimator.SetFloat("Horizontal", 0f);
+            sr.flipY = true;
         }
         // direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         direction = direction.normalized;
