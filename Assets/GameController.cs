df799a613eb1a2370162f91fc826df2ecccf78a8
cristhianbrh <cristhianalexand@gmail.com>
2024-10-17 17:08:49 -0500

    Added menu game over

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class GameController : MonoBehaviour
 {
@@ -10,7 +12,9 @@ public class GameController : MonoBehaviour
     private TMP_Text puntaje;
     private TMP_Text healtText;
     int puntajeanterior = 0;
-    int healtanterior = 0;
+    [SerializeField] int healtanterior = 0;
+    public event EventHandler DeadPlayer;
+
     void Start()
     {
         _dataRepository = new DataRepository();
@@ -34,13 +38,21 @@ public class GameController : MonoBehaviour
     {
         healtanterior = datosJuego.healt;
         datosJuego.healt = datosJuego.healt + healt;
+        if(datosJuego.healt <= 0){
+            DeadPlayer?.Invoke(this, EventArgs.Empty);
+        }
 
+        _dataRepository.SaveGame(datosJuego);
+    }
+    public void resetData(){
+        datosJuego.healt = 5;
+        datosJuego.score = 0;
         _dataRepository.SaveGame(datosJuego);
     }
     // Update is called once per frame
     void Update()
     {
         puntaje.text = "Puntaje: " + datosJuego.score;
-        healtText.text = "Vida: " + datosJuego.healt;
+        healtText.text = "Vida: " + string.Concat(Enumerable.Repeat("❤️", datosJuego.healt));
     }
 }
