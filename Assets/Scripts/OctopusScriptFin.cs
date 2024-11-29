44d61d693305f977cb1f99d2285d8084e2d31681
cristhianbrh <cristhianalexand@gmail.com>
2024-11-28 22:40:57 -0500

    update

--- Assets/Scripts/OctopusScriptFin.cs
+++ Assets/Scripts/OctopusScriptFin.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Necesario para trabajar con escenas
 
 public class OctopusScriptFin : MonoBehaviour
 {
@@ -11,36 +12,46 @@ public class OctopusScriptFin : MonoBehaviour
 
     void Start()
     {
-
+        // Inicialización si es necesario
     }
+
     void Update()
     {
-        if (!isStopped)
-        {
+       
+            // Solo movemos horizontalmente
+            vectorMove.y = 0;
+
+            // Movemos al objeto
             transform.Translate(vectorMove * speed * Time.deltaTime);
-        }
+       
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("WallInvisible"))
+        if (other.CompareTag("WallInvisible") || other.CompareTag("Enemy"))
         {
-            vectorMove = new Vector2(-vectorMove.x, vectorMove.y);
-            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            // Solo cambiar la dirección cuando estamos en la escena "Final_Fight"
+            
+                vectorMove = new Vector2(-vectorMove.x, vectorMove.y);
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            
         }
         if (other.CompareTag("WallInvisible2"))
         {
+            // Detenerse completamente si colisionamos con "WallInvisible2"
             vectorMove = Vector2.zero;
             isStopped = true;
-            // transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
 
         if (other.CompareTag("Bala"))
         {
+            // Reproducir sonido y destruir el objeto si colisiona con una bala
             AudioSource.PlayClipAtPoint(colision, transform.position);
             Destroy(gameObject);
         }
         if (other.CompareTag("Player"))
         {
+            // Si colisiona con el jugador, modificar la salud
             GameObject gameManager = GameObject.Find("GameController");
             gameManager.GetComponent<GameController>().ModifyHealt(-1);
         }
